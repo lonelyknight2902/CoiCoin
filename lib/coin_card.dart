fe3a011823faa2a489ef941dd4a00d701b7b63f1
Thinh Do <truongthinh2902@gmail.com>
2021-12-07 12:26:18 +0700

    api call

---
+++ lib/coin_card.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class CoinCard extends StatelessWidget {
+  //const Coin({ Key? key }) : super(key: key);
+  final String name;
+  final String symbol;
+  final String imageUrl;
+  final double price;
+  final double change;
+  final double changePercentage;
+
+  CoinCard(this.name, this.symbol, this.imageUrl, this.price, this.change,
+      this.changePercentage);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 2.0, 0.0, 0.0),
+            child: AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.network(imageUrl, width: 50),
+                    SizedBox(width: 10),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(name,
+                              style: const TextStyle(
+                                fontSize: 11,
+                                fontWeight: FontWeight.bold,
+                                overflow: TextOverflow.ellipsis,
+                              )),
+                          Text(symbol, style: TextStyle(
+                            fontSize: 10,
+                          )),
+                        ]),
+                  ],
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 2.0, 16.0, 2.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  price.toString(),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 0.25),
+                Row(children: <Widget>[
+                  Text(
+                    change.toString(),
+                    style: TextStyle(
+                      color: change > 0 ? Colors.green : Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 10,
+                    ),
+                  ),
+                  change > 0
+                      ? const Icon(
+                          Icons.arrow_drop_up_rounded,
+                          color: Colors.green,
+                          size: 30,
+                        )
+                      : const Icon(Icons.arrow_drop_down_rounded,
+                          color: Colors.red),
+                ]),
+                //const SizedBox(height: 1.0),
+                Text(
+                  '$changePercentage%',
+                  style: TextStyle(
+                    fontSize: 5,
+                    color: change > 0
+                        ? Colors.green.withOpacity(0.5)
+                        : Colors.red.withOpacity(0.5),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'coin_card.dart';
+import './coin.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List<Coin>> fetchCoin() async {
+    coinList = [];
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      if (values.length > 0) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            coinList.add(Coin.fromJson(map));
+          }
+        }
+      }
+      print(coinList.first.name);
+      setState(() {
+        coinList;
+      });
+
+      return coinList;
+    } else {
+      throw Exception("Failed to load");
+    }
+  }
+
+  @override
+  void initState() {
+    fetchCoin();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: const IconButton(
+            icon: Icon(
+              Icons.account_circle,
+              color: Colors.white,
+            ),
+            onPressed: null,
+          ),
+          title: const Text('CoiCoin'),
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemCount: coinList.length,
+          itemBuilder: (context, index) {
+            return CoinCard(
+                coinList[index].name,
+                coinList[index].symbol,
+                coinList[index].imageUrl,
+                coinList[index].price.toDouble(),
+                coinList[index].change.toDouble(),
+                coinList[index].changePercentage.toDouble());
+          },
+        ));
+  }
+}
