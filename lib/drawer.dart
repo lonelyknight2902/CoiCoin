d9340b43db4a3e84a67d77e88a6b3796e55885b6
talkingTreeHai <haiphamcse@hcmut.edu.vn>
2021-12-07 23:16:31 +0700

    Them nav

---
+++ lib/drawer.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import './home.dart';
+import './account.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(
+      children: [
+        UserAccountsDrawerHeader(
+          accountName: Text('User12345'),
+          accountEmail: Text('abc@hcmut.edu.vn'),
+          currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.blueGrey.shade800,
+              child: ClipOval(
+                child: Image.network(
+                  'https://coin68.com/wp-content/uploads/2021/10/DOGE-Thug-Life.jpg',
+                  width: 45,
+                  height: 45,
+                  fit: BoxFit.cover,
+                ),
+              )),
+        ),
+        ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text('Trang Coin'),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => Account()));
+            }),
+        ListTile(
+            leading: Icon(Icons.money),
+            title: Text('Trang Main'),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => Home()));
+            }),
+      ],
+    ));
+  }
+}
