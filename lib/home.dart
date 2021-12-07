44eba0d0ed3467b1c45b21804eb35994da0d7d80
Thinh Do <truongthinh2902@gmail.com>
2021-12-07 12:53:44 +0700

    add svg library

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http/http.dart' as http;
 import 'coin_card.dart';
 import './coin.dart';
